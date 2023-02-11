c7e520e052381d5f3582c8a5441d7637a464f1cb
pspd0 <rorack@naver.com>
2023-02-12 06:34:56 +0900

    java so bored

--- src/chap_04/_01_If.java
+++ src/chap_04/_01_If.java
@@ -15,5 +15,14 @@ public class _01_If {
             System.out.println("샷추가");
         }
         System.out.println("커피 주문 완료");
+
+        // 오후 2시 이전, 모닝 커피를 마시지 않을 경우?
+        hour = 10;
+        boolean morningCoffee = false;
+
+        if (hour < 14 && morningCoffee == false) {
+            System.out.println("아이스 아메리카노 +1");
+        }
+        System.out.println("커피 주문 완료");
     }
 }
