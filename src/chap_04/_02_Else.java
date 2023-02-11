097cc2f6eb9a14a0f4a875d9a01adcb6d1b2279d
pspd0 <rorack@naver.com>
2023-02-12 06:43:08 +0900

    java so bored

--- src/chap_04/_02_Else.java
+++ src/chap_04/_02_Else.java
@@ -12,5 +12,16 @@ public class _02_Else {
             System.out.println("아이스 아메리카노 (디카페인) +1");
         }
         System.out.println("커피 주문 완료 #1");
+
+        // 오후 2시 이거나 모닝 커피를 마신 경우
+        hour = 15;
+        boolean morningCoffee = true;
+
+        if (hour >= 14 || morningCoffee) {
+            System.out.println("아이스 아메리카노 (디카페인) +1");
+        } else {
+            System.out.println("아이스 아메리카노 +1");
+        }
+        System.out.println("커피 주문 완료 #2");
     }
 }
